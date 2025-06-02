3017f18647f9e2ff45826a566ff6b8fd952dda59
Harry Hartley <hhartley94@gmail.com>
2025-06-02 15:56:44 +0100

    fix text position and add opaque layer

--- src/main.py
+++ src/main.py
@@ -10,14 +10,36 @@ with open('players.csv', newline='') as players:
         name, character, skin = player[0], player[1], player[2]
         
         # Load the character image
-        character = Image.open(f'assets/{character}-{skin}.png')
+        character = Image.open(f'assets/{character}_{skin}.png')
+
+        # Create a semi-transparent overlay
+        overlay = Image.new('RGBA', character.size, (0, 0, 0, 0))
+        overlay_draw = ImageDraw.Draw(overlay)
+        overlay_draw.rectangle((0, 300, 400, 400), fill=(0, 0, 0, 60))
+
+        # Composite the overlay onto the character image
+        character = character.convert('RGBA')  # Ensure character image is in RGBA mode
+        character = Image.alpha_composite(character, overlay)
         
         # Load the font
-        font = ImageFont.truetype("assets/warownia-black-narrow.ttf", 64)
+        fontsize = 70
+        font = ImageFont.truetype("assets/warownia-black-narrow.ttf", fontsize)
         
         # Draw the text on the image
         draw = ImageDraw.Draw(character)
-        draw.text((20, 300), f'{name}', (255, 255, 255), font=font)
+
+        while (width := font.getlength(f'{name}')) > 400:
+            fontsize -= 1
+            font = ImageFont.truetype("assets/warownia-black-narrow.ttf", fontsize)
+
+        text_bbox = draw.textbbox((0, 0), f'{name}', font=font)
+        text_height = text_bbox[3] - text_bbox[1]
+
+        xoffset = (400 - width) // 2
+        yoffset = 300 + (60 - text_height) // 2
+
+        draw.text((xoffset+6, yoffset+6), f'{name}', (0, 0, 0), font=font)
+        draw.text((xoffset, yoffset), f'{name}', (255, 255, 255), font=font)
         
         # Save the modified image
         character.save(f'../output/doombox_{name}.png')
